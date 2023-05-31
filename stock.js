71eedc97adb768b50a969df27483a63f10186fef
SebaDeca <manuelsebastiandecarvalho@gmail.com>
2023-05-31 20:59:51 -0300

    Primer commit

---
+++ stock.js
@@ -0,0 +1,19 @@
+let stockProductos = [
+    { id: 1, nombre: "Buzo G4", tipo: "buzo", cantidad: 1, desc: "Buzo G4 color negro", precio: 12000, talle: "L", img: './img/g4.webp' },
+    { id: 2, nombre: "Polo Label", tipo: "buzo", cantidad: 1, desc: "Buzo Polo negro", precio: 11000, talle: "L", img: './img/polo-label.webp' },
+    { id: 3, nombre: "Bensimon", tipo: "buzo", cantidad: 1, desc: "Buzo Bensimon verde", precio: 12000, talle: "M", img: './img/bensimon.webp' },
+    { id: 4, nombre: "Fraction", tipo: "buzo", cantidad: 1, desc: "Buzo Fraction azul", precio: 14000, talle: "M", img: './img/fraction.webp' },
+    { id: 7, nombre: "Levis", tipo: "remera", cantidad: 1, desc: "Remera Levis verde", precio: 5000, talle: "L", img: './img/levis.webp' },
+    { id: 8, nombre: "Vinson", tipo: "remera", cantidad: 1, desc: "Remera vinson gris", precio: 5000, talle: "L", img: './img/vinson.webp' },
+    { id: 9, nombre: "Bravo", tipo: "remera", cantidad: 1, desc: "Remera Bravo salmón", precio: 5000, talle: "M", img: './img/bravo.webp' },
+    { id: 10, nombre: "Boardwise", tipo: "remera", cantidad: 1, desc: "Remera Boardwise blanca", precio: 7000, talle: "M", img: './img/boardwise.webp' },
+    { id: 13, nombre: "Levis Classic", tipo: "camisa", cantidad: 1, desc: "Camisa negra Levis Classic", precio: 9000, talle: "L", img: './img/levis-classic.webp' },
+    { id: 14, nombre: "Tascani ", tipo: "camisa", cantidad: 1, desc: "Camisa Tascani amarilla", precio: 14000, talle: "S", img: './img/tascani.webp' },
+    { id: 15, nombre: "Rever Pass", tipo: "camisa", cantidad: 1, desc: "Camisa Rever Pass jean", precio: 70000, talle: "L", img: './img/rever-pass.webp' },
+    { id: 16, nombre: "Prototype", tipo: "camisa", cantidad: 1, desc: "Camisa Prototype roja", precio: 7770, talle: "S", img: './img/prototype.webp' },
+    { id: 19, nombre: "Winterfex", tipo: "pantalon", cantidad: 1, desc: "Jean Winterfex negro", precio: 16000, talle: "L", img: './img/winterfex.webp' },
+    { id: 20, nombre: "Wrangler", tipo: "pantalon", cantidad: 1, desc: "Jean Wrangler", precio: 32000, talle: "L", img: './img/wrangler.webp' },
+    { id: 21, nombre: "Airborn", tipo: "pantalon", cantidad: 1, desc: "Jean Airborn chupin", precio: 23000, talle: "M", img: './img/airborn.webp' },
+    { id: 22, nombre: "Brooksfield", tipo: "pantalon", cantidad: 1, desc: "Jean Brooksfield azul oscuro", precio: 56000, talle: "M", img: './img/brooksfield.webp' },
+   
+]
